c8fdbb8f3c57e23b4dec595612f66d27f25dabd3
DiegoCata <difercata26@gmail.com>
2020-11-18 02:57:45 -0500

    MetodoResta

--- src/Operacion.java
+++ src/Operacion.java
@@ -1,3 +1,6 @@
+
+import java.util.Scanner;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -9,5 +12,15 @@
  * @author Diego
  */
 public class Operacion {
-    
+    public static float restarNumeros() {
+        System.out.println("RESTA DE DOS NUMEROS");
+        float numero1, numero2;
+        Scanner s = new Scanner(System.in);
+        System.out.print("Ingrese Número 1: ");
+        numero1 = Float.valueOf(s.nextLine());
+        System.out.printf("Ingrese Número 2: ");
+        numero2 = Float.valueOf(s.nextLine());
+        System.out.printf("La Resta es: ");
+        return numero1 - numero2;
+    }
 }
